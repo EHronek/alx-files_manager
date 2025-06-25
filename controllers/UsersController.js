8655d2cdb01e3cfc28e61fba57051bc81b9cfb77
EHronek <emmanuelhronek@gmail.com>
2025-06-25 12:02:33 +0300

    3. Create a new user 1

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,17 @@
-// import sha1 from 'sha1';
-import crypto from 'crypto';
+// controllers/UsersController.js
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
-    // check if email exists
+    // Check for missing email
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
 
-    // check for missing password
+    // Check for missing password
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
@@ -19,16 +19,16 @@ class UsersController {
     try {
       const usersCollection = dbClient.client.db().collection('users');
 
-      // check if email exists
+      // Check if email already exists
       const existingUser = await usersCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({ error: 'Already exist' });
       }
 
-      // hash tje password
-      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+      // Hash the password
+      const hashedPassword = sha1(password);
 
-      // Insert new User
+      // Insert new user
       const result = await usersCollection.insertOne({
         email,
         password: hashedPassword,
@@ -40,10 +40,8 @@ class UsersController {
         email,
       });
     } catch (error) {
-      console.error(`Error creating user: ${error}`);
-      return res.status(500).json({
-        error: 'Internal Server Error',
-      });
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
 }
