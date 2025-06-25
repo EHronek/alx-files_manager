ae84067a478de2c1cc49fef17464e41fd6bd9b5b
EHronek <emmanuelhronek@gmail.com>
2025-06-25 12:08:27 +0300

    3. Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,22 +1,50 @@
-// controllers/UsersController.js
+// import sha1 from 'sha1';
 import crypto from 'crypto';
 import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body || {};
+    const { email, password } = req.body;
 
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
+    // check if email exists
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
 
-    const userExists = await dbClient.db.collection('users').findOne({ email });
-    if (userExists) return res.status(400).json({ error: 'Already exist' });
+    // check for missing password
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
 
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    try {
+      const usersCollection = dbClient.client.db().collection('users');
 
-    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+      // check if email exists
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
 
-    return res.status(201).json({ id: result.insertedId.toString(), email });
+      // hash tje password
+      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+      // Insert new User
+      const result = await usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      // Return the new user (without password)
+      return res.status(201).json({
+        id: result.insertedId,
+        email,
+      });
+    } catch (error) {
+      console.error(`Error creating user: ${error}`);
+      return res.status(500).json({
+        error: 'Internal Server Error',
+      });
+    }
   }
 }
 
